e758a9773fc9b9ab3e8d4e5651470ec32a2992cb
andre <andre@192.168.0.9>
2023-06-27 07:31:11 -0300

    Primer commit

---
+++ src/Logica/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica;
+
+import GUI.PantallaULP;
+
+/**
+ *
+ * @author andre
+ */
+public class Principal {
+    public static void main(String[] args) {
+        PantallaULP panta = new PantallaULP();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
